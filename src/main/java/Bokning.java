ea588b8a18930410ad3609848031490835352e07
MarcusKarlberg <Marcus.Karlberg@yh.nackademin.se>
2018-02-05 13:38:19 +0100

    dto klasser

---
+++ src/main/java/Bokning.java
@@ -0,0 +1,42 @@
+
+import java.util.List;
+
+public class Bokning {
+    private int id;
+    private List<Medlem> medlemmar;
+    private Pass pass;
+
+    public Bokning() {
+    }
+
+    public Bokning(int id, List<Medlem> medlemmar, Pass pass) {
+        this.id = id;
+        this.medlemmar = medlemmar;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Medlem> getMedlemmar() {
+        return medlemmar;
+    }
+
+    public void setMedlemmar(List<Medlem> medlemmar) {
+        this.medlemmar = medlemmar;
+    }
+
+    public Pass getPass() {
+        return pass;
+    }
+
+    public void setPass(Pass pass) {
+        this.pass = pass;
+    }
+ 
+}
---
+++ src/main/java/Notis.java
@@ -0,0 +1,50 @@
+public class Notis {
+    private int id;
+    private String kommentar;
+    private Anstalld anstalld;
+    private Medlem medlem;
+
+    public Notis() {
+    }
+
+    public Notis(int id, String kommentar, Anstalld anstalld, Medlem medlem) {
+        this.id = id;
+        this.kommentar = kommentar;
+        this.anstalld = anstalld;
+        this.medlem = medlem;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public Anstalld getAnstalld() {
+        return anstalld;
+    }
+
+    public void setAnstalld(Anstalld anstalld) {
+        this.anstalld = anstalld;
+    }
+
+    public Medlem getMedlem() {
+        return medlem;
+    }
+
+    public void setMedlem(Medlem medlem) {
+        this.medlem = medlem;
+    }
+    
+    
+}
