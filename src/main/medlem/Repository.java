acb14e7e07a17800c0b48eacbf27eb310e03c7b3
stanley-nackademin <stanley.ho@yh.nackademin.se>
2018-02-05 14:34:07 +0100

    Lagt till Repository-klass

--- src/main/medlem/Repository.java
+++ src/main/medlem/Repository.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Repository {
     private String username;
@@ -8,8 +9,18 @@ public class Repository {
     private String mysqlAddress;
 
     public Repository() {
-        try (FileInputStream settings = new FileInputStream("settings.properties")) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        Properties p = new Properties();
+        try (FileInputStream file = new FileInputStream("settings.properties")) {
+            p.load(file);
+            username = p.getProperty("username");
+            password = p.getProperty("password");
+            mysqlAddress = p.getProperty("mysqlAddress");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
