46cebbbcfc7be2cd5389618e9f8e89080d7388f3
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2018-02-05 16:34:50 +0100

    hje

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,8 @@
 public class Test {
     
-    public static void main(String[] arg){
+    public static void main(String[] args){
 
-        System.out.println("Hello world Amanda");
+        System.out.println("Hej jag heter claudia");
     }
 
 
