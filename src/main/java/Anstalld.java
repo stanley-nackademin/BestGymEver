305d6013f1316ac3066baff187a1edba8fa389a9
MarcusKarlberg <Marcus.Karlberg@yh.nackademin.se>
2018-02-05 13:11:43 +0100

    DTO Klasser

---
+++ src/main/java/Anstalld.java
@@ -0,0 +1,60 @@
+public class Anstalld {
+    private int id;
+    private String aNamn;
+    private String losen;
+    private int behörighet;
+    private int personalRegister;
+
+    public Anstalld() {
+    }
+
+    public Anstalld(int id, String aNamn, String losen, int behörighet, int personalRegister) {
+        this.id = id;
+        this.aNamn = aNamn;
+        this.losen = losen;
+        this.behörighet = behörighet;
+        this.personalRegister = personalRegister;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getaNamn() {
+        return aNamn;
+    }
+
+    public void setaNamn(String aNamn) {
+        this.aNamn = aNamn;
+    }
+
+    public String getLosen() {
+        return losen;
+    }
+
+    public void setLosen(String losen) {
+        this.losen = losen;
+    }
+
+    public int getBehörighet() {
+        return behörighet;
+    }
+
+    public void setBehörighet(int behörighet) {
+        this.behörighet = behörighet;
+    }
+
+    public int getPersonalRegister() {
+        return personalRegister;
+    }
+
+    public void setPersonalRegister(int personalRegister) {
+        this.personalRegister = personalRegister;
+    }
+    
+    
+}
---
+++ src/main/java/Traningstyp.java
@@ -0,0 +1,29 @@
+public class Traningstyp {
+    private int id;
+    private String namn;
+
+    
+    public Traningstyp() {
+    }
+
+    public Traningstyp(int id, String namn) {
+        this.id = id;
+        this.namn = namn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }  
+}
