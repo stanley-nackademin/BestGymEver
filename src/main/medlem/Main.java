7e4d79717550edfd75d3b4c55a93d28adeda7e5a
stanley-nackademin <stanley.ho@yh.nackademin.se>
2018-02-06 16:13:35 +0100

    Lagt till pass- och inloggningsmeny

--- src/main/medlem/Main.java
+++ src/main/medlem/Main.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
-
-    private Controller controller = new Controller();
+    private MemberMenu memberMenu = new MemberMenu();
 
     Main() {
-        Scanner input = new Scanner(System.in);
-        while (true) {
-            System.out.println("-- Medlemsinloggning --");
-            System.out.print("Skriv in ditt användarnamn: ");
-            String username = input.nextLine();
-            System.out.print("Skriv in ditt lösenord: ");
-            String password = input.nextLine();
-            controller.userLogin(username, password);
-        }
+        memberMenu.mainMenu();
     }
 
     public static void main(String[] args) {
---
+++ src/main/medlem/MemberMenu.java
@@ -0,0 +1,70 @@
+import DTO.Medlem;
+
+import java.util.Scanner;
+
+public class MemberMenu {
+    private Repository repo;
+    Medlem member = null;
+
+    public MemberMenu() {
+        repo = new Repository();
+    }
+
+    public void mainMenu() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("-- Medlemsinloggning --");
+        System.out.print("Skriv in ditt användarnamn: ");
+        String username = input.nextLine().trim();
+        System.out.print("Skriv in ditt lösenord: ");
+        String password = input.nextLine().trim();
+        member = repo.getMemberByLogin(username, password);
+
+        if (member != null) {
+            System.out.println("Du är inloggad som " + member.getaNamn());
+            workoutMenu();
+        } else {
+            System.out.println("Kunde ej logga in.\n");
+        }
+    }
+
+    private void workoutMenu() {
+        boolean loop = true;
+        Scanner input = new Scanner(System.in);
+
+        while (loop) {
+            System.out.println();
+            System.out.println("[1] Visa tillgängliga pass");
+            System.out.println("[2] Visa mina bokade pass");
+            System.out.println("[3] Boka ett pass");
+            System.out.println("[4] Avboka ett pass");
+            System.out.print("Skriv in ditt val: ");
+            String workoutMenuChoice = input.nextLine().trim();
+
+            switch (workoutMenuChoice) {
+                case "1":
+                    showAvailableWorkouts();
+                    break;
+                case "2":
+                    System.out.println("2");
+                    break;
+                case "3":
+                    System.out.println("3");
+                    break;
+                case "4":
+                    System.out.println("4");
+                    break;
+                case "q":
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("\nOgiltig val");
+                    break;
+            }
+        }
+    }
+
+    private void showAvailableWorkouts() {
+
+    }
+}
