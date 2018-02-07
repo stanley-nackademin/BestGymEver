16fc72d7cfc266e46beefff97d388f717ee6d9c8
Amanda <amanda@iwanowski.com>
2018-02-07 18:53:19 +0100

    .

--- src/main/java/Repo/RepositoryReception.java
+++ src/main/java/Repo/RepositoryReception.java
@@ -16,36 +16,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author claudiastenberg
- */
-//public class RepositoryReception {
-//          
-//    public List <Bokning>getBokningByID(int medlem_id, int pass_id){
-//        List <Bokning> bokning = new ArrayList<>(); 
-//        String query = "Select Medlem_id, Pass_id from Bokning";
-//        
-//        try (
-//                  Connection con1 = DriverManager.getConnection(p.getProperty("connectionString"),
-//                         p.getProperty("name"),
-//                         p.getProperty("password"));
-//         Statement stmt = con1.createStatement();
-//         ResultSet rs = stmt.executeQuery(query);){
-//
-//        while (rs.next()){
-//            int id = rs.getInt("id");
-//            //getPersonByMedlemId(id);
-//            int passID = rs.getInt("passId");
-//            //bokning.add(new Bokning(id,passID)));
-//        }
-//        
-//        //IF medlem id and pass id finns 
-//        //checka in person
-//    
-//    }   catch (SQLException ex) {
-//            Logger.getLogger(RepositoryAdmin.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return null;
-//}
-//}
+    public class RepositoryReception {
+
+
+
+}
--- src/main/medlem/Main.java
+++ src/main/medlem/Main.java
@@ -7,6 +7,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
         Main m = new Main();
     }
 }
