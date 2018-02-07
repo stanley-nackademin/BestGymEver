8054e718833a0398d2b72a2637cdaaf18ada57a5
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2018-02-07 16:10:24 +0100

    hej

--- src/main/java/Controller/ControllerReception.java
+++ src/main/java/Controller/ControllerReception.java
@@ -7,6 +7,7 @@ package Controller;
 
 import DTO.Medlem;
 import DTO.Bokning;
+import DTO.Pass;
 import DTO.Person;
 import Repo.RepositoryAdmin;
 import java.sql.SQLException;
@@ -17,23 +18,42 @@ import java.util.List;
  *
  * @author claudiastenberg
  */
-public class ControllerReception {
-    
-    RepositoryAdmin rn = new RepositoryAdmin();
-    
-    public boolean matchMedlemsID(int ID) throws SQLException {
-        boolean finns = false;
-        Person person = null;
-        int id = 0;
-        List<Person> personer = new ArrayList<>();
-
-        person = rn.getPersonByMedlemId(id);
-
-        if(person != null)
-            finns = true;
-
-        return finns;
-    }
-    
-    
-}
+//public class ControllerReception {
+//    
+//    RepositoryAdmin rn = new RepositoryAdmin();
+//    
+//    public boolean matchBokning(int PassId, int MedlemsId) throws SQLException {
+//        boolean finns = false;
+//        Pass pass = null;
+//        Medlem medlem = null;
+//        
+//        int id = 0;
+//        List<Bokning> bokningar = new ArrayList<>();
+//
+//        bokningar = rn.get
+//        
+//        person = rn.getPersonByMedlemId(id);
+//
+//        if(person != null)
+//            finns = true;
+//
+//        return finns;
+//    }
+//    
+//    
+//
+//public boolean matchAnstalldUsername(String name) throws SQLException {
+//        boolean finns = false;
+//        Anstalld anstalld = null;
+//        int id = 0;
+//        List<Anstalld> personal = new ArrayList<>();
+//
+//        personal = ra.getAllAnstallda();
+//        anstalld = personal.stream().filter(c -> c.getaNamn().equalsIgnoreCase(name)).findFirst().orElse(null);
+//
+//        if(anstalld != null)
+//            finns = true;
+//
+//        return finns;
+//    }
+//}
--- src/main/java/Repo/RepositoryReception.java
+++ src/main/java/Repo/RepositoryReception.java
@@ -6,13 +6,21 @@
 package Repo;
 
 import DTO.Bokning;
+import DTO.Medlem;
+import DTO.Pass;
+import DTO.Sal;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,11 +28,100 @@ import java.util.logging.Logger;
  *
  * @author claudiastenberg
  */
-//public class RepositoryReception {
-//          
-//    public List <Bokning>getBokningByID(int medlem_id, int pass_id){
-//        List <Bokning> bokning = new ArrayList<>(); 
-//        String query = "Select Medlem_id, Pass_id from Bokning";
+public class RepositoryReception {
+          
+    
+    RepositoryAdmin ra = new RepositoryAdmin();
+    Pass pass = new Pass();
+    
+      private Connection con;
+    private Properties p = new Properties();
+    
+    public RepositoryReception(){
+        try {
+            p.load(new FileInputStream("/Users/marcuskarlberg/Desktop/Gruppuppgift/BestGymEver.properties"));
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+    }
+    
+    public Pass getPassIdByBokningsId(int id) throws SQLException {
+        Pass pass = null;
+        String query = "Select Pass_id from Bokning "
+                + "inner join Pass on Pass.id = bokning.Pass_id"
+                + "where bokning.Pass_id = ?";
+        
+        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"),
+        p.getProperty("name"),
+        p.getProperty("password"));
+        PreparedStatement pStmt = con.prepareStatement(query);){
+        pStmt.setString(1, id+"");
+        ResultSet rs = pStmt.executeQuery();
+        
+        while(rs.next()) {
+            pass = new Pass(id, rs.getBoolean("privat"), rs.getDate("datum"), rs.getInt("deltagande"), 
+                    ra.getTraningstypByPassId(id), ra.getSalByPassId(id), 
+                    ra.getAnstalldByPassId(id), ra.getTidsLuckorByPassId(id));
+        }
+  
+}       
+    catch (SQLException ex){
+            Logger.getLogger(RepositoryReception.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return pass;
+
+    }
+  public Medlem getMedlemIdByBokningsId(int id){
+      Medlem medlem = null;
+      String query = "";
+  
+      try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"),
+        p.getProperty("name"),
+        p.getProperty("password"));
+        PreparedStatement pStmt = con.prepareStatement(query);){
+        pStmt.setString(1, id+"");
+        ResultSet rs = pStmt.executeQuery();
+  
+  
+  
+  }     catch (SQLException ex) {  
+            Logger.getLogger(RepositoryReception.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+    
+    return null;
+
+  }
+}
+
+//   public Sal getSalByPassId(int id) {
+//        Sal sal = null;
+//        String query = "select Sal.id, Sal.namn, Sal.platser from Sal "
+//                + "inner join Pass on Pass.Sal_id = Sal.id where Pass.id = ?;";
+//
+//    try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"),
+//        p.getProperty("name"),
+//        p.getProperty("password"));
+//        PreparedStatement pStmt = con.prepareStatement(query);){
+//        pStmt.setString(1, id+"");
+//        ResultSet rs = pStmt.executeQuery();
+//
+//        while(rs.next()){
+//           sal = new Sal(rs.getInt("id"), rs.getString("namn"), rs.getInt("platser"));
+//        }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//        
+//        return sal;
+//    }
+//}
+//    
+//    public List <Bokning>getAllBokningar(int medlem_id, int pass_id){
+//        List <Bokning> allabokning = new ArrayList<>(); 
+//      
+//        String query = "Select * from Bokningar";
 //        
 //        try (
 //                  Connection con1 = DriverManager.getConnection(p.getProperty("connectionString"),
@@ -35,9 +132,8 @@ import java.util.logging.Logger;
 //
 //        while (rs.next()){
 //            int id = rs.getInt("id");
-//            //getPersonByMedlemId(id);
-//            int passID = rs.getInt("passId");
-//            //bokning.add(new Bokning(id,passID)));
+////            Pass passID = rs.getString("passID");
+//            allabokning.add(new Bokning(id, ra.getAllMedlemmar(), pass.getId()));
 //        }
 //        
 //        //IF medlem id and pass id finns 
